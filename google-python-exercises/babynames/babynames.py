94dc32c74244f782447eb3bad399a21a782a5c4b
shannon buckley <buckley.shannont@gmail.com>
2016-03-12 10:31:35 -0800

    update babynames. It works!
    takes file or files
    takes optional flag to output .summary with rows of alphabetical names for each file provided
    expects files of text with a certain string identifying the year, ranks and names within td elements

--- google-python-exercises/babynames/babynames.py
+++ google-python-exercises/babynames/babynames.py
@@ -8,6 +8,9 @@
 
 import sys
 import re
+import os
+from os import path
+import shutil
 
 """Baby Names exercise
 
@@ -34,35 +37,104 @@ Suggested milestones for incremental development:
  -Fix main() to use the extract_names list
 """
 
+
 def extract_names(filename):
-  """
-  Given a file name for baby.html, returns a list starting with the year string
-  followed by the name-rank strings in alphabetical order.
-  ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
-  """
-  # +++your code here+++
-  return
+    """
+    Given a file name for baby.html, returns a list starting with the year string
+    followed by the name-rank strings in alphabetical order.
+    ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
+    """
+
+    names_file = path.join(filename)
+
+    names_list = []
+
+    try:
+        f = open(names_file, 'rU')
+
+        text = f.read()
+    except IOError:
+        return
+
+    year_pattern = r'in\s\d\d\d\d<'
+
+    year_match = re.search(year_pattern, text)
+    print year_match.group().strip('in <')
+
+    if year_match:
+        year = year_match.group().strip('in <')
+        names_list.append(year)
+
+    name_rank_pat = '<td>([\d]+)</td><td>(\w+)</td><td>(\w+)</td>'
+
+    name_rank_match = re.findall(name_rank_pat, text)
+
+    if name_rank_match:
+        ranks = [rank for rank in name_rank_match]
+
+        #print ranks
+
+        names_dict = {}
+        for rank in ranks:
+            names_dict[rank[0]] = [rank[1], rank[2]]
+
+        #print names_dict
+
+        for k, v in names_dict.items():
+
+            names_list.append(v[0] + ' ' + str(k))
+            names_list.append(v[1] + ' ' + str(k))
+
+    #print sorted(names_list)
+
+    return sorted(names_list)
 
 
 def main():
-  # This command-line parsing code is provided.
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
+    # This command-line parsing code is provided.
+    # Make a list of command line arguments, omitting the [0] element
+    # which is the script itself.
+    args = sys.argv[1:]
 
-  if not args:
-    print 'usage: [--summaryfile] file [file ...]'
-    sys.exit(1)
+    if not args:
+        print 'usage: [--summaryfile] file [file ...]'
+        sys.exit(1)
 
-  # Notice the summary flag and remove it from args if it is present.
-  summary = False
-  if args[0] == '--summaryfile':
-    summary = True
-    del args[0]
+    # Notice the summary flag and remove it from args if it is present.
+    summary = False
+    if args[0] == '--summaryfile':
+        summary = True
+        del args[0]
 
-  # +++your code here+++
-  # For each filename, get the names, then either print the text output
-  # or write it to a summary file
+    # +++your code here+++
+    # For each filename, get the names, then either print the text output
+    # or write it to a summary file
+    for filename in args:
+
+        file_path = path.abspath(filename)
+
+        if path.exists(file_path):
+
+            baby_names = extract_names(filename)
+
+        else:
+
+            print 'file %s does not exist' % filename
+            continue
+
+        if summary:
+
+                new_filename = filename.replace('.html', '.summary')
+                print new_filename
+                g = open(path.join(new_filename), 'wb')
+
+                g.writelines(' \n'.join(baby_names))
+                g.close()
+
+        else:
+            print baby_names[0]  # year
+            for name in baby_names[1:]:
+                print name
   
 if __name__ == '__main__':
-  main()
+    main()
