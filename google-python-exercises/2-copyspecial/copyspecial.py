f27386728c98049b6a22aea7d471809fedd9f828
shannon buckley <buckley.shannont@gmail.com>
2016-05-29 11:28:20 -0700

    add hints

--- google-python-exercises/2-copyspecial/copyspecial.py
+++ google-python-exercises/2-copyspecial/copyspecial.py
@@ -22,6 +22,13 @@ The copyspecial.py program takes one or more directories as its arguments.
 We'll say that a "special" file is one where the name contains the pattern __w__ somewhere, where the w is one or more
 word chars. The provided main() includes code to parse the command line arguments, but the rest is up to you.
 Write functions to implement the features below and modify main() to call your functions.
+
+Suggested functions for your solution(details below):
+
+get_special_paths(dir) -- returns a list of the absolute paths of the special files in the given directory
+copy_to(paths, dir) given a list of paths, copies those files into the given directory
+zip_to(paths, zippath) given a list of paths, zip those files up into the given zipfile
+
 """
 
 # +++your code here+++
