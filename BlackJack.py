d90200c02cd4b5c79c49b4da5d92838c282cfbe9
shannon buckley <buckley.shannont@gmail.com>
2016-09-18 08:39:57 -0700

    add Game class to Blackjack, add methods to run Game, adjust __str__ methods, Hand class still in prog., adjust main to use Game, add files to gitignore

--- BlackJack.py
+++ BlackJack.py
@@ -4,11 +4,12 @@ __author__ = , 9/11/16
 """
 
 import random
+import sys
 
 
 class Game:
 
-    CHOICES = []
+    CHOICES = ['hit', 'stand', 'deal (forfeits current hand)']
 
     def __init__(self):
         self.game_on = False
@@ -17,28 +18,97 @@ class Game:
         self.deck = Deck()
         self.dealer_score = 0
         self.player_score = 0
+        self.outcome = ""
 
     def deal_cards(self):
 
         if self.game_on:
+            self.outcome = 'Player forfeited game and started a new one!'
+            print self.outcome
             self.player_score -= 1
+            self.game_on = False
+        else:
+            self.deck = Deck()
+            self.deck.shuffle_cards()
+            self.player_hand.add_card(self.deck.deal_card())
+            self.dealer_hand.add_card(self.deck.deal_card())
+            self.player_hand.add_card(self.deck.deal_card())
+            self.dealer_hand.add_card(self.deck.deal_card())
 
-        self.game_on = True
-        self.deck = Deck()
-        self.deck.shuffle_cards()
-        self.player_hand.add_card(self.deck.deal_card())
-        self.dealer_hand.add_card(self.deck.deal_card())
-        self.player_hand.add_card(self.deck.deal_card())
-        self.dealer_hand.add_card(self.deck.deal_card())
-
-        print 'Dealer showing: '
-        print self.player_hand
+            print 'Dealer showing: {}'.format(self.dealer_hand.cards[0])
+            # print 'Player has: {}'.format(self.player_hand.cards)
+            print 'Player has: {}, {} \n\tValue is {}'.format(self.player_hand.cards[0], self.player_hand.cards[1],
+                                                              self.player_hand.get_value())
+            self.game_on = True
+            self.outcome = self.player_choice()
 
     def hit(self):
-        self.player_hand.add_card(self.deck.deal_card())
+
+        if self.game_on:
+
+            if self.player_hand.get_value() <= 21:
+
+                self.player_hand.add_card(self.deck.deal_card())
+
+            print self.player_hand.cards[0:len(self.player_hand.cards)-1]
+
+            if self.player_hand.get_value() > 21:
+
+                self.outcome = "Player Busted!"
+                print(self.outcome)
+
+                self.player_score -= 1
+                self.game_on = False
+
+            print self.player_hand.get_value()
+            self.player_choice()
 
     def stand(self):
-        pass
+
+        self.game_on = False
+
+        while self.dealer_hand.get_value() < 17:
+            self.dealer_hand.add_card(self.deck.deal_card())
+
+        print 'Dealer has: {}'.format(self.dealer_hand.get_value())
+
+        if self.dealer_hand.get_value() > 21:
+            self.outcome = "\n{}\nDealer Busted! PLAYER WINS!".format(72*'=')
+            self.player_score += 1
+            print self.outcome
+            print "Play Again?"
+
+        else:
+            if self.dealer_hand.get_value() >= self.player_hand.get_value() or self.player_hand.get_value() > 21:
+                self.outcome = "Dealer Wins! :("
+                self.player_score -= 1
+            else:
+                self.outcome = "Player wins!"
+                self.player_score += 1
+
+            print self.outcome
+            self.player_choice()
+
+    def player_choice(self):
+
+        try:
+
+            choice = raw_input('What do you do? Choices = {}'.format(Game.CHOICES))
+
+            while choice in Game.CHOICES:
+                if choice == 'hit':
+                    self.hit()
+                elif choice == 'stand':
+                    self.stand()
+                elif choice == 'deal':
+                    self.deal_cards()
+
+            print '\nPlayer has chosen to {}'.format(choice)
+            return choice
+
+        except KeyboardInterrupt:
+            print '\nExiting...'
+            sys.exit()
 
 
 class Card:
@@ -64,7 +134,7 @@ class Card:
         return self.suit
 
     def __str__(self):
-        return 'The {} of {}'.format(self.rank, self.suit)
+        return self.rank + self.suit
 
 
 class Deck:
@@ -79,7 +149,7 @@ class Deck:
 
         for suit in self.SUITS:
             for rank in self.RANKS:
-                self.cards.append(rank + suit)
+                self.cards.append(Card(rank, suit))
 
     def __str__(self):
 
@@ -92,6 +162,7 @@ class Deck:
     def deal_card(self):
 
         card = self.cards.pop()
+
         return card
 
 
@@ -104,42 +175,56 @@ class Hand:
 
     def __str__(self):
 
-        return '{}'.format(self.cards)
+        return self.cards[0]
 
     def add_card(self, card):
 
         self.cards.append(card)
 
+    def get_value(self):
+
+        hand_value = 0
+
+        has_ace = False
+
+        for card in self.cards:
+
+            rank = card.get_rank()
+
+            hand_value += Card.VALUES[rank]
+
+            if rank == 'A':
+
+                has_ace = True
+
+        if hand_value < 11 and has_ace:
+            hand_value += 10
+
+        return hand_value
+
+    def check_blackjack(self):
+
+        if 'A' in self.cards:
+            if '10' or 'J' or 'Q' or 'K' in self.cards:
+
+                print '\nBlackJack!'
+
 
 def main():
 
-    deck = Deck()
+    g = Game()
 
-    print deck
+    g.deal_cards()
 
-    deck.shuffle_cards()
+    while not g.game_on:
 
-    print deck
+        print "what would you like to do?\nChoices:\t{}".format(" ".join(Game.CHOICES))
 
-    hand_one = Hand()
+        player_choice = raw_input("\n > ")
 
-    hand_one.add_card(deck.deal_card())
-
-    dealer_hand = Hand()
-
-    dealer_hand.add_card(deck.deal_card())
-
-    hand_one.add_card(deck.deal_card())
-
-    dealer_hand.add_card(deck.deal_card())
-
-    print deck
-
-    print 'Player has: %s' % hand_one.cards
-
-    print 'Dealer showing: %s' % dealer_hand.cards[0]
-
-    print dealer_hand
+        if 'hit' in player_choice:
+            if g.game_on:
+                pass
 
 
 if __name__ == '__main__':
