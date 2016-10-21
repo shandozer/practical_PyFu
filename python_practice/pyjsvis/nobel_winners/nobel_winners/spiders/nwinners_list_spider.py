9baf64a153d6adecbdf70cd73e5a4411adbad568
shannon buckley <buckley.shannont@gmail.com>
2016-10-20 20:00:09 -0700

    nobel_winners: add scrapy spider and some example output json

---
+++ python_practice/pyjsvis/nobel_winners/nobel_winners/spiders/nwinners_list_spider.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+__author__ = ShannonB., 10/2/16
+"""
+
+import scrapy
+import re
+
+
+class NWinnerItem(scrapy.Item):
+    country = scrapy.Field()
+    name = scrapy.Field()
+    link_text = scrapy.Field()
+
+
+class NWinnerSpider(scrapy.Spider):
+
+    name = 'nwinners_list'
+    allowed_domains = ['en.wikipedia.org']
+    start_urls = [
+        "http://en.wikipedia.org/wiki/List_of_Nobel_laureates_by_country"
+    ]
+
+    def parse(self, response):
+
+        h2s = response.xpath('//h2')
+
+        for h2 in h2s:
+
+            country = h2.xpath('span[@class="mw-headline"]/text()').extract()
+            if country:
+                winners = h2.xpath('following-sibling::ol[1]')
+                for w in winners.xpath('li'):
+                    text = w.xpath('descendant-or-self::text()').extract()
+                    yield NWinnerItem(
+                        country=country[0],
+                        name=text[0],
+                        link_text=' '.join(text)
+                    )
+
+    def next_method(self):
+        pass
